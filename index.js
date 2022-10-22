5ac29e7ee5ac9769a6f8c820e491c38aafaf9709
Kapoul-85 <108029583+Kapoul-85@users.noreply.github.com>
2022-10-22 20:14:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const intents = new Discord.IntentsBitField(3276799);
 const bot = new Discord.Client({intents});
 const loadCommands = require("./Loaders/loadCommands");
 const loadEvents = require("./Loaders/loadEvents");
-const token = require("./token.json");
 
 bot.commands = new Discord.Collection();
 bot.login(process.env.TOKEN);
